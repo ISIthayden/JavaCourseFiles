faa3102f9ddf8dee8914256d6c49aa6dcfc0f249
Sasha <sasha@Sashas-MacBook-Pro.local>
2019-10-04 19:10:43 -0700

    completed section 4

---
+++ SECTION 4/CalcYearsAndDays/src/CalcYearsAndDays.java
@@ -0,0 +1,26 @@
+/*Write a method printYearsAndDays with parameter of type long named minutes.
+        The method should not return anything (void) and it needs to calculate the years
+         and days from the minutes parameter.
+        If the parameter is less than 0, print text "Invalid Value".
+        Otherwise, if the parameter is valid then it needs to print a message in the format
+         "XX min = YY y and ZZ d".
+
+        XX represents the original value minutes.
+        YY represents the calculated years.
+        ZZ represents the calculated days.
+
+        EXAMPLES OF INPUT/OUTPUT:
+        * printYearsAndDays(525600);  → should print "525600 min = 1 y and 0 d"*/
+
+public class CalcYearsAndDays {
+    public static void printYearsAndDays(long minutes){
+        if(minutes < 0){
+            System.out.println("Invalid Value");
+        }
+        if(minutes >= 0){
+            int years = (int) (((minutes / 60) / 24) / 365);
+            int days = (int) ((minutes - (years * 365 * 24 * 60)) / 60 / 24);
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
