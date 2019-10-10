8fa208d567ec7e6f3e87e9ffb028fdead290cc07
Tony Hayden <proximality24@gmail.com>
2019-10-09 17:00:04 -0700

    updated files 10-9

---
+++ SECTION 5/PerfectNumber/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 28;
+        int num2 = 24;
+        int num3 = 76;
+
+        System.out.println("Is " + num1 + " a perfect number? " + PerfectNumber.isPerfectNumber(num1));
+        System.out.println("Is " + num2 + " a perfect number? " + PerfectNumber.isPerfectNumber(num2));
+        System.out.println("Is " + num3 + " a perfect number? " + PerfectNumber.isPerfectNumber(num3));
+
+    }
+}
