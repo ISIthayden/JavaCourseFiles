00f6881086e0f5452fb838e17e4c85c862288233
tony hayden <proximality24@gmail.com>
2019-10-09 18:11:01 -0700

    updated files 10-9

---
+++ SECTION 5/ForLoop/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+
+        for(int i = 2; i < 9; i++){
+            System.out.println("10000 at " + i + "% interest = " + calculateInterest(10000, i));
+        }
+        for(int i = 8; i > 1; i--){
+            System.out.println("10000 at " + i + "% interest = " + calculateInterest(10000, i));
+        }
+
+        for(int i = 10000; i < 20000; i++){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int n){
+
+        if(n == 1) {
+            return false;
+        }
+
+        for(int i = 2; i <= (long) Math.sqrt(n); i++){
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static double calculateInterest(double amount, double interestRate){
+        return(amount *(interestRate/100));
+    }
+}
