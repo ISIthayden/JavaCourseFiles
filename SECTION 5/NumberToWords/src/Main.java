08cea6d24f597908700c2486d3f901f18f5dfc38
tony hayden <proximality24@gmil.com>
2019-10-14 11:18:53 -0700

    updated files

---
+++ SECTION 5/NumberToWords/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(NumberToWords.getDigitCount(4565));
+        System.out.println(NumberToWords.reverse(123400));
+        NumberToWords.numberToWords(1234000);
+    }
+}
